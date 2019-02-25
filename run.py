6dd98c9fde8017f8ce9fe27eec40a06320dff723
Sylwester <sylwesterpiorkowski@yahoo.ie>
2019-02-25 13:15:59 +0000

    Create form template, POST method, provide feedback to user with flash

--- run.py
+++ run.py
@@ -1,8 +1,9 @@
 import os
 import json
-from flask import Flask, render_template
+from flask import Flask, render_template, request, flash
 
 app = Flask(__name__)
+app.secret_key = 'some_secret'
 
 @app.route('/')
 def index():
@@ -26,8 +27,14 @@ def about_member(member_name):
                 member = obj
     return render_template("member.html", member=member)
         
-@app.route('/contact')
+@app.route('/contact', methods=["GET", "POST"])
 def contact():
+    if request.method == "POST":
+        flash("Thanks {}, we have received your message!".format(
+            request.form["name"]))
+        
+        # print(request.form)
+        
     return render_template("contact.html", page_title="Contact")
     
 @app.route('/careers')
